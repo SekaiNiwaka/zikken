97c1b98fc6ef616d7dc073d84762086518581d89
SekaiNiwaka <kantaro061026@outlook.jp>
2025-08-26 11:19:06 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 import os
 from flask import Flask, render_template
+
+# WebSocketの通信が正しく行われるようにするために、
+# geventとgevent-websocketをインポートします
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
 from flask_socketio import SocketIO, emit
+
+# geventのパッチを適用します
+monkey.patch_all()
 
 app = Flask(__name__)
 
@@ -15,7 +25,9 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # トップページにアクセスされたときにindex.htmlをレンダリングします
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # RenderのホストURLをHTMLテンプレートに渡します
+    host_url = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+    return render_template('index.html', host_url=host_url)
 
 # クライアントが接続したときに実行されます
 @socketio.on('connect')
